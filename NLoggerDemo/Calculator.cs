32363d6275b0df8c7e483f5ef7fb12ba67e9f8a1
Vivek Verma <vverma240420@gmail.com>
2023-07-06 18:45:18 +0530

    Added NLoggerProgram

---
+++ NLoggerDemo/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLoggerDemo
+{
+    internal class Calculator
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public int CalculateSum(int a, int b)
+        {
+            logger.Info($"Calculating the sum of {a} and {b}.");
+            int sum = a + b;
+            logger.Info($"The sum of {a} and {b} is {sum}.");
+            return sum;
+
+        }
+    }
+}
---
+++ NLoggerDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+
+namespace NLoggerDemo
+{
+    internal class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+              logger.Info("Program Started");
+              try
+              {
+                  Console.WriteLine("Enter the first number: ");
+                  int num1= int.Parse(Console.ReadLine());
+
+                  Console.WriteLine("Enter the second number: ");
+                  int num2= int.Parse(Console.ReadLine());
+
+                  Calculator c= new Calculator();
+                  int sum= c.CalculateSum(num1, num2);
+                  Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+              }
+              catch (Exception ex) 
+              {
+                  logger.Error(ex, "Error Found");
+                  Console.WriteLine("Error Found");
+              }
+              logger.Info("Program Finished");
+              Console.ReadLine();  
+        }
+    }
+}
